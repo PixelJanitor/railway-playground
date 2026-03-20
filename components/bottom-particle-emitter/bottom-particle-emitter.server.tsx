e87a9a72133b389d05e9eb7b55f393c08b47e1f7
Derek Briggs <derek.briggs@me.com>
2026-03-20 17:07:27 -0600

    chore: remove bottom particle emitter

--- components/bottom-particle-emitter/bottom-particle-emitter.server.tsx
+++
@@ -1,5 +0,0 @@
-import { BottomParticleEmitterClient } from "@/components/bottom-particle-emitter/bottom-particle-emitter.client";
-
-export async function BottomParticleEmitter() {
-  return <BottomParticleEmitterClient />;
-}
